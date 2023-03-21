3e1706f9b88228d8182ace430bd47a6312c51833
antti.saarela <antti.saarela@tietoevry.com>
2023-03-21 12:29:39 +0000

    unique name for fl hs orch

--- create_orchestrator.bat
+++ create_orchestrator.bat
@@ -1,7 +1,7 @@
 az deployment group create --template-file ^
  ./mlops/bicep/modules/fl_pairs/vnet_compute_storage_pair.bicep ^
  --resource-group rg-amlhs-dev ^
-  --parameters pairBaseName="orchestrator" pairRegion="northeurope" ^
+  --parameters pairBaseName="flhsorch" pairRegion="northeurope" ^
   machineLearningName="mlw-amlhs-dev" machineLearningRegion="northeurope" ^
   subnetPrefix="10.0.100.0/24" ^
   storagePublicNetworkAccess=Enabled
